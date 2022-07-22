7da3b319a6d51735e2a51d701e4959b0f8308f70
C-Max <coder.max@yahoo.com>
2022-07-22 09:16:05 +0300

    footer ready pochti

--- src/components/TableComponent/Table.js
+++ src/components/TableComponent/Table.js
@@ -1,44 +1,62 @@
-import { useEffect,useState } from 'react';
-import { getPostListSelector } from '../store/selectors/selectors';
+import { useEffect, useState } from 'react';
+import { getAllPages } from '../store/selectors/selectors';
 import { useSelector } from 'react-redux'
 import { HeaderComponent, TableComponents } from './components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './style.scss'
 
 
 
 
 export function Table(){
-    const posts = useSelector(getPostListSelector())
-    const [currentPosts, setCurrentPosts] = useState([]); 
+    const pages = useSelector(getAllPages());
+    const [coursePage, setCoursePage] = useState(0)
+    const [disabledButtons, setDisabledButtons] = useState({back: true, next: false})
 
-
-    const selectPosts = ( from = 0,size = 10)=>{
-        let partPosts = [];
-        if(posts.length === 0){
-            setCurrentPosts(new Array(10))
-        }
-        if(posts.length < size){
-            size = posts.length;
-        }
-        for(let i = from; i < 10; i++){
-            partPosts.push(posts[i]);
-        }
-        
-        setCurrentPosts([...partPosts]);
+    const hundleNextPage = () => {
+        setCoursePage(coursePage + 1);
     }
-    
+
+    const hundleBackPage = () => {
+        setCoursePage(coursePage - 1);
+    }
 
     useEffect(()=>{
-        selectPosts()
-    },[])
+
+        if(coursePage === 0){
+            setDisabledButtons({back: true, next: false});
+
+        }else if(coursePage === pages.length - 1){
+            setDisabledButtons({back: false, next: true})
+
+        }else{
+            setDisabledButtons({back: false, next: false})
+        }
+
+
+
+    },[coursePage])
 
     return (
         <>
+
         <HeaderComponent/>
-        <TableComponents postList={currentPosts}/>
-        <nav>
-            <button className='button__back'>Назад</button>
-            <button className='button__next'>Вперед</button>
+        <TableComponents postList={pages[coursePage].posts}/>
+
+
+
+        <nav className='nav'>
+            <button 
+            className={`nav__button nav__button_next ${disabledButtons.back && "disabled"}`}
+            onClick={hundleBackPage}>
+                Назад
+            </button>
+            
+            <button 
+            className={`nav__button nav__button_next ${disabledButtons.next && "disabled"}`}
+            onClick={hundleNextPage}>
+                Вперед
+            </button>
         </nav>
         </>
     )
--- src/components/store/selectors/selectors.js
+++ src/components/store/selectors/selectors.js
@@ -2,3 +2,7 @@ export const getPostListSelector = (from = 0,size = 10) => (state)=>{
    const posts = state.posts;
    return posts;
 }
+
+export const getAllPages = ()=> (state) => {
+   return state.pages;
+}
